ef9d3f6e3453dcc0ebe5cc79bb9259b4cef41cb1
Beta-OSS <beta214365@gmail.com>
2025-07-28 12:31:17 +0800

    submission fixes

---
+++ routes.py
@@ -0,0 +1,26 @@
+# routes.py
+from flask import Blueprint, request, render_template
+from flags import check_flag
+
+bp = Blueprint('main', __name__)
+
+@bp.route("/")
+def index():
+    return render_template("index.html")
+
+@bp.route("/challenges")
+def challenges():
+    return render_template("challenge.html")
+
+@bp.route("/submit")
+def submit_flag():
+    challenge_id = request.args.get("challenge_id")
+    flag = request.args.get("flag")
+
+    if not challenge_id or not flag:
+        return "Incorrect", 400
+
+    if check_flag(challenge_id, flag):
+        return "Correct"
+    else:
+        return "Incorrect"
